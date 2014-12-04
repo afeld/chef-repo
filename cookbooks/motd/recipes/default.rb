f0cba8ee114c6df3f42a6c755cb886adc0a7f919
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-04 17:43:46 +0000

    add template for motd

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,9 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
-file "/etc/motd" do
-  action :create
-  content "Property of 18F"
+template "/etc/motd" do
+  source "motd.erb"
   mode "0644"
   owner "root"
   group "root"
