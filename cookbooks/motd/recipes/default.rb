d5e0ff8c048172c77395db7dcbebf3a15df71ba1
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-04 17:32:02 +0000

    update motd recipe

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+file "/etc/motd" do
+  action :create
+  content "Property of 18F"
+  mode "0644"
+  owner "root"
+  group "root"
+end
