1509be156991fce9422c4159f9da81218a71821b
Aidan Feldman <aidan.feldman@gsa.gov>
2014-12-04 15:28:43 -0500

    add pending specs for apache

--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -5,4 +5,14 @@ describe 'apache' do
   it "is awesome" do
     expect(true).to eq(true)
   end
+
+  it "is installed"
+
+  it "is running"
+
+  it "is responding to http requests"
+
+  it "is displaying the proper home page"
+
+  it "is running on the default port"
 end
